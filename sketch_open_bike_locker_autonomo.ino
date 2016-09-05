c1f3dea0db5e95a4e72db3971ef4dd9580fa5e51
Dario Buma <d.buma@activevideo.com>
2016-09-05 15:17:00 +0200

    - Added relay for opening Ronald's kluis - Added code for the use case of blocking locker after a certain timeout (2 mins)

--- sketch_open_bike_locker_autonomo.ino
+++ sketch_open_bike_locker_autonomo.ino
@@ -58,6 +58,7 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 // define for servo motor (A0)
 #define SERVO_PIN 4
+#define RELAY_PIN 12
 
 
 /* defines for low level lock API (in uplink direction) 
@@ -72,7 +73,7 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);
 * 
 * CMD_SET_LOCKER_STATE sets state of the locker
 * command = 1 byte
-* param1 1 byte (required) state => maps 1:1 to veiligstallen; 0 = free, 1 = bezet, 2 = geblokkeerd, 3 = not used (gereserveerd), 4 =  buiten werking 
+* param1 1 byte (required) state => maps 1:1 to veiligstallen; 0 = vrij, 1 = bezet, 2 = geblokkeerd, 3 = gereserveerd, 4 =  buiten werking 
 * param2 10 bytes (optional) mifareid
 * 
 * CMD_UPDATE_LOCKER_USER_ID sets the mifareid of the locker that is currently used (eg. anonymity expires after 24h)
@@ -82,15 +83,18 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);
 #define CMD_GET_SERVER_TIME 1
 #define CMD_INVOKE_DOWNLINK 2
 #define CMD_SET_LOCKER_STATE 3
-#define CMD_UPDATE_LOCKER_USER_ID 4
+#define CMD_REQUEST_LOCKER_UNBLOCK 4
 
 //Function for simulation of lock (servo based, can alco be a relay)
 void openLock()
 {
+  unsigned long timeout=millis();
     // poor man's PWM, SG90 servo needs about 50Hz pwm
     pinMode(SERVO_PIN, OUTPUT);     
     digitalWrite(VCC_SW, HIGH);     
  
+  digitalWrite(RELAY_PIN, HIGH);
+    
     for (uint16_t zz=0;zz<20;zz++)
     {  
         digitalWrite(SERVO_PIN,HIGH);
@@ -109,6 +113,11 @@ void openLock()
 
     digitalWrite(VCC_SW, LOW);     
     pinMode(SERVO_PIN, INPUT);     
+
+  while((millis()-timeout)<5000)
+    delay(100);
+ 
+  digitalWrite(RELAY_PIN, LOW);
 }
 
 // Functions, variables for led control
@@ -183,23 +192,25 @@ uint8_t readSwitch(uint8_t switchId)
 
 // Functions, variables for keeping/setting the state of the locker 
 uint8_t lockerInUse=0;
-uint8_t storedUid[10];
+unsigned long lockerInUseTime=0;
+const uint8_t uidLength=4;
+uint8_t storedUid[uidLength];
 
 void uidCopy(uint8_t *buf, uint8_t offset, uint8_t *uid)
 {
-  for (int zz=0;zz<10;zz++)
+  for (int zz=0;zz<uidLength;zz++)
     buf[zz+offset]=uid[zz];
 }
 
 void uidStore(uint8_t *uid)
 {
-  for (int zz=0;zz<10;zz++)
+  for (int zz=0;zz<uidLength;zz++)
     storedUid[zz]=uid[zz];
 }
 
 uint8_t uidCompare(uint8_t *uid)
 {
-  for (int zz=0;zz<10;zz++)
+  for (int zz=0;zz<uidLength;zz++)
     if (storedUid[zz]!=uid[zz])
       return 0;
   return 1;
@@ -207,8 +218,14 @@ uint8_t uidCompare(uint8_t *uid)
 
 void uidShow(uint8_t *uid)
 {
-  for (int zz=0;zz<10;zz++)
-    debugSerial.print(uid[zz]);
+  for (int zz=0;zz<uidLength;zz++)
+  { 
+    if (zz)
+      debugSerial.print('.');
+    if (uid[zz]<16)
+      debugSerial.print('0');
+    debugSerial.print(uid[zz],HEX);
+  }
 }
 
 // Functions, variables for thingsnetwork (ABP assumed)
@@ -221,6 +238,31 @@ uint8_t receivePayload[32];
 uint16_t receivedBytes;
 uint8_t sendRetries=0;
 
+void rawRN2483Cmd(const uint8_t* command)
+{
+  uint8_t t=0;
+  debugSerial.print("CMD : ");
+  while (command[t])
+  {
+    debugSerial.write(command[t]);
+    loraSerial.write(command[t++]);
+  }
+  
+  unsigned long time=millis();;
+
+  while(((millis()-time)<1000)&&(!loraSerial.available()))
+    delay(10);
+
+  if (loraSerial.available())
+  {
+    debugSerial.print("RSP : ");
+    while (loraSerial.available()) 
+      debugSerial.write(loraSerial.read());
+  }
+  else
+    debugSerial.println("No response!");
+}
+
 void clearReceivePayload(void)
 {
   for (uint8_t zz;zz<32;zz++)
@@ -230,6 +272,14 @@ void clearReceivePayload(void)
 uint8_t ttnSend(const uint8_t* payload, uint8_t size, uint8_t reqAck=0, uint8_t retries=0)   
 {  
   uint8_t resp, ret=0;
+
+/*  rawRN2483Cmd((uint8_t*)"radio get freq\r\n");
+  rawRN2483Cmd((uint8_t*)"radio get sync\r\n");
+  rawRN2483Cmd((uint8_t*)"radio get cr\r\n");
+  rawRN2483Cmd((uint8_t*)"radio get sf\r\n");
+  rawRN2483Cmd((uint8_t*)"radio get crc\r\n");
+  rawRN2483Cmd((uint8_t*)"radio get prlen\r\n");
+*/
   if (reqAck)
     resp=LoRaBee.sendReqAck(1,payload,size,retries);
   else
@@ -297,6 +347,9 @@ void setup()
   pinMode(VCC_SW, OUTPUT);     
   digitalWrite(VCC_SW, LOW);     
 
+  // initialise relay
+  pinMode(RELAY_PIN, OUTPUT);
+  
   // initialise status leds
   pinMode(LED_BUILTIN, OUTPUT);     
   pinMode(LED_RED, OUTPUT);     
@@ -341,7 +394,6 @@ void loop()
     if (ttnSend(sendPayload,1,1))
     {
       debugSerial.println("Acknowledge packet received!");
-      ledBlink(LED_GREEN,3);
       
       clearReceivePayload();
       receivedBytes=LoRaBee.receive(receivePayload,32);
@@ -351,7 +403,7 @@ void loop()
       	debugSerial.print(receivedBytes);
       	debugSerial.print(" byte(s) : ");
       	for (uint8_t zz=0;zz<receivedBytes;zz++)
-      	  debugSerial.println(receivePayload[zz]);
+          debugSerial.println(receivePayload[zz],HEX);
       	
       	if ((receivePayload[0]==1)&&(receivePayload[1]==0xff))
       	{
@@ -360,12 +412,16 @@ void loop()
           ledSet(LED_GREEN,1);
 
           lockerInUse=0;
+          lockerInUseTime=0;
           openLock();
 
           sendPayload[0]=CMD_SET_LOCKER_STATE;
           sendPayload[1]=0; // vrij
-          ttnSend(sendPayload,2);
+          if (ttnSend(sendPayload,2,1))
+            ledBlink(LED_GREEN,3);
       	}
+        else
+          ledBlink(LED_GREEN,1);
       }
       else    
       	debugSerial.println("No return data received!");
@@ -386,51 +442,133 @@ void loop()
       debugSerial.println("");  
       //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 
-      if (!lockerInUse)
+      switch(lockerInUse)
       {
+        case 0: // locker vrij
         debugSerial.println("Locker is now in use!");
         uidStore((uint8_t *)mfrc522.uid.uidByte);
         ledSet(LED_RED,1);
         ledSet(LED_GREEN,0);
 
         lockerInUse=1;
+          lockerInUseTime=millis();
         openLock();
 
         sendPayload[0]=CMD_SET_LOCKER_STATE;
         sendPayload[1]=1; // bezet
-        uidCopy(sendPayload,2,(uint8_t *)mfrc522.uid.uidByte);
-        ttnSend(sendPayload,12);
+          //uidCopy(sendPayload,2,(uint8_t *)mfrc522.uid.uidByte);
+          ttnSend(sendPayload,2,1);
+  
+          delay(2000); // guard time
+          break;
+        case 1: // locker bezet
+          if (uidCompare((uint8_t *)mfrc522.uid.uidByte))
+          {
+            debugSerial.println("Locker is no longer in use!");
+
+            ledSet(LED_RED,0);
+            ledSet(LED_GREEN,1);
+  
+            lockerInUse=0;
+            lockerInUseTime=0;
+            openLock();
+  
+            sendPayload[0]=CMD_SET_LOCKER_STATE;
+            sendPayload[1]=0; // vrij
+            //uidCopy(sendPayload,2,(uint8_t *)mfrc522.uid.uidByte);
+            ttnSend(sendPayload,2,1);
 
         delay(2000); // guard time
       }
       else
       {
+            debugSerial.print("Safe is in use by another card (");
+            uidShow((uint8_t *)storedUid);
+            debugSerial.println(")!");
+            
+            ledBlink(LED_RED,5);
+          }
+          break;
+        case 2: // locker geblokkeerd
         if (uidCompare((uint8_t *)mfrc522.uid.uidByte))
         {
-          debugSerial.println("Locker is no longer in use!");
+            debugSerial.println("Locker is blocked, requesting open lock from server!");
+            sendPayload[0]=CMD_REQUEST_LOCKER_UNBLOCK;
+            uidCopy(sendPayload,1,(uint8_t *)mfrc522.uid.uidByte);
+            if (ttnSend(sendPayload,1+uidLength,1))
+            {
+              debugSerial.println("Acknowledge packet received!");
+      
+              clearReceivePayload();
+              receivedBytes=LoRaBee.receive(receivePayload,32);
+              if(receivedBytes)
+              {  
+                debugSerial.print("I received ");
+                debugSerial.print(receivedBytes);
+                debugSerial.print(" byte(s) : ");
+                for (uint8_t zz=0;zz<receivedBytes;zz++)
+                  debugSerial.println(receivePayload[zz]);
+                
+                if ((receivePayload[0]==1)&&(receivePayload[1]==0xff))
+                {
+                   debugSerial.println("Unblock clearance received from downlink, locker is no longer in use!");
+  
           ledSet(LED_RED,0);
           ledSet(LED_GREEN,1);
 
           lockerInUse=0;
+                    lockerInUseTime=0;
           openLock();
 
+                    lockerInUse=0;
+                    lockerInUseTime=0;
           sendPayload[0]=CMD_SET_LOCKER_STATE;
           sendPayload[1]=0; // vrij
-          uidCopy(sendPayload,2,(uint8_t *)mfrc522.uid.uidByte);
-          ttnSend(sendPayload,12);
-
-          delay(2000); // guard time
+                    if (ttnSend(sendPayload,2,1))
+                      ledBlink(LED_GREEN,3);
+                  }
+                  else
+                    ledBlink(LED_GREEN,5);
+                }
+                else    
+                  debugSerial.println("No return data received!");
+              }
         }
         else
         {
           debugSerial.print("Safe is in use by another card (");
           uidShow((uint8_t *)storedUid);
           debugSerial.println(")!");
+            
           ledBlink(LED_RED,5);
         }
+            break;
+          default:
+            // do nothing
+            break;
       }
     }
     delay(500);
   }
+  
+  if (lockerInUse==1)
+  {
+    if ((millis()-lockerInUseTime)>120000) // block the safe after 2 minutes (24 hours)
+    {
+      debugSerial.println("Locker is now blocked!");
+      lockerInUse=2;
+      lockerInUseTime=0;
+      
+      ledSet(LED_RED,1);
+      ledSet(LED_GREEN,1);
+
+      sendPayload[0]=CMD_SET_LOCKER_STATE;
+      sendPayload[1]=2; // geblokkeerd
+      uidCopy(sendPayload,2,(uint8_t *)mfrc522.uid.uidByte);
+      if (ttnSend(sendPayload,2+uidLength,1))
+  ledBlink(LED_GREEN,3);
+
+    }
+  }   
 }
 
